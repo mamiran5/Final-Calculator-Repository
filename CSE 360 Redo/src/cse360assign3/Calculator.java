8c2f66097c5cb47b6e8a18a80b47c9b1fe858296
Marqwon Miranda <mamiran5@asu.edu>
2016-02-21 17:51:28 -0700

    This is my third attempt at properly using Git and GitHub

---
+++ CSE 360 Redo/src/cse360assign3/Calculator.java
@@ -0,0 +1,81 @@
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/** This is a constructor that allows for a new value to be calculated from 0.
+	*
+	* @param none No parameters
+	* @return none No return value
+	*/
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** This returns the final value obtained after calculations
+	*
+	* @param none No parameters
+	* @return int returns the total value calculated
+	*/
+	
+	public int getTotal()
+	{
+		return 0;
+	}
+	
+	/** This adds a number to the current total
+	*
+	* @param int The value to be added
+	* @return none No return value
+	*/
+	
+	public void add (int value)
+	{
+
+	}
+	
+	/** This subtracts a number from the current total
+	*
+	* @param int The value to be subtracted
+	* @return none No return value
+	*/
+	
+	public void subtract (int value)
+	{
+
+	}
+	
+	/** This multiplies the current total by a number
+	*
+	* @param int The value to multiply the total by
+	* @return none No return value
+	*/
+	
+	public void multiply (int value)
+	{
+
+	}
+	
+	/** This divides the current total by a number
+	*
+	* @param int The value the total will be divided by
+	* @return none No return value
+	*/
+	
+	public void divide (int value)
+	{
+
+	}
+	
+	/** This prints the entire history of the calculations done on the total
+	*
+	* @param none No return value
+	* @return String Returns the history of the calculations as a string to see what was done on the total
+	*/
+	
+	public String getHistory () {
+		return "";
+	}
+}
